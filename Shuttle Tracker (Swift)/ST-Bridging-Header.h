ca3ca70c20bcef3ae3c236231f98c70554411056
Benn Linger <benn@linger.com>
2015-01-19 00:16:01 -0500

    Added Reachability pod. Implemented Objective-C bridging header.

---
+++ Shuttle Tracker (Swift)/ST-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Pods-Bridging-Header.h
+//  Shuttle Tracker (Swift)
+//
+//  Created by Benn Linger on 1/19/15.
+//  Copyright (c) 2015 Seton Hill University. All rights reserved.
+//
+
+#ifndef Shuttle_Tracker__Swift__Pods_Bridging_Header_h
+#define Shuttle_Tracker__Swift__Pods_Bridging_Header_h
+
+#import <Reachability/Reachability.h>
+
+#endif
