a1baebeb210652d1a4aaa218b1bb8485c9ff1944
Benn Linger <benn@linger.com>
2015-01-19 14:39:41 -0500

    Corrected back bar button item for detail view controller to have no title.

--- Shuttle Tracker (Swift)/STShuttleDetailTableViewController.swift
+++ Shuttle Tracker (Swift)/STShuttleDetailTableViewController.swift
@@ -12,6 +12,8 @@ class STShuttleDetailTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .Plain, target: nil, action: nil)
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
