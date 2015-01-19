6fa317646848513ac906a8ab485e824db220f52b
Benn Linger <benn@linger.com>
2015-01-19 09:50:08 -0500

    Implemented placeholder view controller classes. Added to main storyboard.

---
+++ Shuttle Tracker (Swift)/STMapViewController.swift
@@ -0,0 +1,37 @@
+//
+//  STMapViewController.swift
+//  Shuttle Tracker (Swift)
+//
+//  Created by Benn Linger on 1/19/15.
+//  Copyright (c) 2015 Seton Hill University. All rights reserved.
+//
+
+import UIKit
+
+class STMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Seton Hill Shuttles";
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
