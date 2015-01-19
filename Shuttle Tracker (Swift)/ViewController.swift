587c4342883d0901e8977f6dacca1e4fd63bb1b1
Benn Linger <benn@linger.com>
2015-01-18 22:57:42 -0500

    Initial commit. Fresh project.

---
+++ Shuttle Tracker (Swift)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shuttle Tracker (Swift)
+//
+//  Created by Benn Linger on 1/18/15.
+//  Copyright (c) 2015 Seton Hill University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
