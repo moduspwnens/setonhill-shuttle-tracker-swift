736e1dd5933502a3d3038dc543d31051f6570821
Benn Linger <benn@linger.com>
2015-02-04 21:12:22 -0500

    Implemented table view section for shuttle schedules. Showing the schedules not yet implemented.

--- Shuttle Tracker (Swift)/STMasterTableViewController.swift
+++ Shuttle Tracker (Swift)/STMasterTableViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 let kShuttleTableSectionIndex = 0
+let kSchedulesTableSectionIndex = 1
 
 let kTableViewShuttleCellReuseIdentifier = "kTableViewShuttleCellReuseIdentifier"
+let kTableViewScheduleCellReuseIdentifier = "kTableViewScheduleCellReuseIdentifier"
 
 let kShuttleSelectedNotification = "kShuttleSelected"
 
@@ -39,6 +41,15 @@ class STMasterTableViewController: UITableViewController, UISplitViewControllerD
             name: kVisibleShuttlesUpdatedNotification,
             object: nil
         )
+        
+        // Set up listener for when the shuttle schedule links (from remote configuration variables) are updated.
+        NSNotificationCenter.defaultCenter().addObserverForRemoteConfigurationUpdate(
+            self,
+            selector: "shuttleScheduleLinksChanged:",
+            name: "ShuttleScheduleLinks",
+            object: nil
+        )
+        
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -81,13 +92,16 @@ class STMasterTableViewController: UITableViewController, UISplitViewControllerD
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // Return the number of sections.
-        return 1
+        return 2
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
         if section == kShuttleTableSectionIndex {
             return self.visibleShuttleArray.count
+        }
+        else if section == kSchedulesTableSectionIndex {
+            return self.getShuttleScheduleLinks().count
         }
         
         return 0
@@ -98,6 +112,9 @@ class STMasterTableViewController: UITableViewController, UISplitViewControllerD
         if section == kShuttleTableSectionIndex {
             return NSLocalizedString("Shuttles", comment:"")
         }
+        else if section == kSchedulesTableSectionIndex {
+            return NSLocalizedString("Schedules", comment:"")
+        }
         return nil
     }
     
@@ -107,6 +124,8 @@ class STMasterTableViewController: UITableViewController, UISplitViewControllerD
         var cell : UITableViewCell?
         
         if indexPath.section == kShuttleTableSectionIndex {
+            // This is a table view cell for a specific shuttle.
+            
             cell = tableView.dequeueReusableCellWithIdentifier(kTableViewShuttleCellReuseIdentifier) as? UITableViewCell
             if cell == nil {
                 cell = UITableViewCell(style: .Subtitle, reuseIdentifier: kTableViewShuttleCellReuseIdentifier)
@@ -116,6 +135,18 @@ class STMasterTableViewController: UITableViewController, UISplitViewControllerD
             cell?.textLabel?.text = thisShuttle.title
             cell?.detailTextLabel?.text = thisShuttle.subtitle
             cell?.imageView?.image = UIImage(named: thisShuttle.getBlipImageName())
+        }
+        else if indexPath.section == kSchedulesTableSectionIndex {
+            // This is a table view cell for a shuttle schedule link.
+            
+            cell = tableView.dequeueReusableCellWithIdentifier(kTableViewScheduleCellReuseIdentifier) as? UITableViewCell
+            if cell == nil {
+                cell = UITableViewCell(style: .Default, reuseIdentifier: kTableViewScheduleCellReuseIdentifier)
+            }
+            
+            let thisScheduleLink = self.getShuttleScheduleLinks()[indexPath.row] as [String:String]
+            cell?.textLabel?.text = thisScheduleLink["title"]!
+            cell?.accessoryType = .DisclosureIndicator
         }
 
         return cell!
@@ -127,8 +158,8 @@ class STMasterTableViewController: UITableViewController, UISplitViewControllerD
         
         // What action we'll take depends on what section the selected row is in.
         if indexPath.section == kShuttleTableSectionIndex {
-            
             // The user tapped a shuttle's table cell.
+            
             let thisShuttle = self.visibleShuttleArray[indexPath.row]
             
             // Post notification that the shuttle was selected.
@@ -144,6 +175,17 @@ class STMasterTableViewController: UITableViewController, UISplitViewControllerD
             if self.splitViewController!.collapsed {
                 self.splitViewController?.showDetailViewController(self.detailViewController, sender: self)
             }
+            
+            // De-select this cell.
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        }
+        else if indexPath.section == kSchedulesTableSectionIndex {
+            // The user tapped the name of a shuttle schedule.
+            
+            let thisScheduleLink = self.getShuttleScheduleLinks()[indexPath.row] as [String:String]
+            let scheduleName = thisScheduleLink["title"]
+            
+            println("Schedule selected: \(scheduleName)")
             
             // De-select this cell.
             tableView.deselectRowAtIndexPath(indexPath, animated: true)
@@ -165,6 +207,17 @@ class STMasterTableViewController: UITableViewController, UISplitViewControllerD
         
         // Tell table view to reload the table section that shows shuttles.
         self.tableView.reloadSections(NSIndexSet(index: kShuttleTableSectionIndex), withRowAnimation: .None)
+    }
+    
+    func shuttleScheduleLinksChanged(notification: NSNotification) {
+        // Tell table view to reload the table section that shows shuttle schedules.
+        self.tableView.reloadSections(NSIndexSet(index: kSchedulesTableSectionIndex), withRowAnimation: .None)
+    }
+    
+    // MARK: - Convenience methods
+    
+    func getShuttleScheduleLinks() -> [AnyObject] {
+        return NSUserDefaults.standardUserDefaults().arrayForKey("ShuttleScheduleLinks")!
     }
 
 }
